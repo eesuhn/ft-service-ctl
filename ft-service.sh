196e3570f8a1f81cc4a94876b5e4f92a9f9e1de7
eesuhn <eason.yihong@gmail.com>
2024-06-07 12:33:18 +0800

    Added main script

---
+++ ft-service.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+PURPLE=$(tput setaf 5)
+NC=$(tput sgr0)
+
+if [ $# -eq 0 ]; then
+	echo "Usage: fs <service>"
+	exit 1
+fi
+
+SERVICE="$1"
+STATUS=$(systemctl is-active $SERVICE)
+
+check_status() {
+	if [ $STATUS = "active" ]; then
+		echo "${SERVICE} is ${GREEN}active${NC}"
+		read -p "Do you want to stop ${SERVICE}? [y/n] " RESPONSE
+	else
+		echo "${SERVICE} is ${RED}inactive${NC}"
+		read -p "Do you want to start ${SERVICE}? [y/n] " RESPONSE
+	fi
+}
+
+init_service() {
+	if [ $STATUS = "active" ]; then
+		echo "${SERVICE} is already ${GREEN}initialized${NC}"
+		exit 1
+	fi
+
+	sudo systemctl start $SERVICE
+	if [ $? -eq 0 ]; then
+		echo "${SERVICE} has been ${GREEN}initialized${NC}"
+	else
+		echo "${SERVICE} failed to start"
+		exit 1
+	fi
+}
+
+stop_service() {
+	if [ $STATUS = "inactive" ]; then
+		echo "${SERVICE} is already ${RED}stopped${NC}"
+		exit 1
+	fi
+
+	sudo systemctl stop $SERVICE
+	if [ $? -eq 0 ]; then
+		echo "${SERVICE} has been ${RED}stopped${NC}"
+	else
+		echo "${SERVICE} failed to stop"
+		exit 1
+	fi
+}
+
+reload_service() {
+	if [ $STATUS = "inactive" ]; then
+		echo "${SERVICE} can't be reloaded because it is ${RED}stopped${NC}"
+		exit 1
+	fi
+
+	sudo systemctl reload $SERVICE
+	if [ $? -eq 0 ]; then
+		echo "${SERVICE} has been ${PURPLE}reloaded${NC}"
+	else
+		echo "${SERVICE} failed to reload"
+		exit 1
+	fi
+}
+
+if [ "$2" == "-i" ]; then
+	init_service
+elif [ "$2" == "-s" ]; then
+	stop_service
+elif [ "$2" == "-r" ]; then
+	reload_service
+else
+	check_status
+	if [[ $RESPONSE =~ ^[Yy]$ ]]; then
+		if [ $STATUS = "active" ]; then
+			stop_service
+		else
+			init_service
+		fi
+	fi
+fi
